f3095ac99b1b4f7732078f02369444a18e5b6872
likhon15 <likhon15-3916@diu.edu.bd>
2024-05-29 03:58:53 +0600

    added admin

--- job_board/models.py
+++ job_board/models.py
@@ -7,3 +7,6 @@ class JobPosting(models.Model):
     company = models.CharField(max_length=50)
     salary = models.IntegerField()
     is_active = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.title} | {self.company} | {self.is_active}"
