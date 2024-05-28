351e2c73628546b730bc2c34b471acb1542a93a3
likhon15 <likhon15-3916@diu.edu.bd>
2024-05-28 17:09:50 +0600

    added models for db

--- job_board/models.py
+++ job_board/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class JobPosting(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    company = models.CharField(max_length=50)
+    salary = models.IntegerField()
