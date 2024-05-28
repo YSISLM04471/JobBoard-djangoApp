a11c91c9f86c7e2ab97ff0f41eee85c71e30e862
likhon15 <likhon15-3916@diu.edu.bd>
2024-05-29 02:58:43 +0600

    added

--- job_board/urls.py
+++ job_board/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, details
 
 urlpatterns = [
     path('', index),
+    path('jobs/<int:pk>/', details, name="job-detail")
 ]
--- job_board/views.py
+++ job_board/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
+from .models import JobPosting
 
 def index(request):
-    return HttpResponse("Hello World!")
+    job = JobPosting.objects.filter(is_active=True)
+    print(job)
+    context = {
+        'jobs': job
+    }
+    return render (request, 'job_board/index.html', context)
+
+
+def details(request, pk):
+    job = get_object_or_404(JobPosting, pk=pk, is_active=True)
+    context = {
+        'singleJobs': job
+    }
+    return render (request, 'job_board/details.html', context)
